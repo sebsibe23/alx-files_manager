0963d379f28d7a660c7826dc44030ef1fb29abcf
sebsibe23 <sebsibe1990@proton.me>
2024-07-01 13:50:14 -0700

    server.js

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
+/**
+ * Initialize a new Express server instance.
+ */
 const server = express();
 
+/**
+ * Inject middlewares into the server instance.
+ * @param {object} server - The Express server instance.
+ */
 injectMiddlewares(server);
+
+/**
+ * Inject routes into the server instance.
+ * @param {object} server - The Express server instance.
+ */
 injectRoutes(server);
+
+/**
+ * Start the server with the injected middlewares and routes.
+ * @param {object} server - The Express server instance.
+ */
 startServer(server);
 
 export default server;
