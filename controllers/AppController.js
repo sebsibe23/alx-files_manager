07bf6a8ea503ffa3f295010e4d03d8c9d169e56e
sebsibe23 <sebsibe1990@proton.me>
2024-07-01 13:52:23 -0700

    controllers/AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,18 +2,41 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
+/**
+ * Represents the controller for handling application status and statistics.
+ */
 export default class AppController {
+  /**
+   * Retrieves the current status of Redis and MongoDB connections.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
   static getStatus(req, res) {
-    res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
+    try {
+      res.status(200).json({
+        redis: redisClient.isAlive(),
+        db: dbClient.isAlive(),
+      });
+    } catch (error) {
+      console.error('Failed to get status:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
+  /**
+   * Retrieves statistics including the number of users and files in the database.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
   static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+    try {
+      Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+        .then(([usersCount, filesCount]) => {
+          res.status(200).json({ users: usersCount, files: filesCount });
+        });
+    } catch (error) {
+      console.error('Failed to get statistics:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
